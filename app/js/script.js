0796f1e9afd5bc679196415f2af964725da1e184
Tkachyshen Andrei (Workpc) <tkachyshen.a@gmail.com>
2022-02-18 11:09:56 +0200

    add forum page

--- app/js/script.js
+++ app/js/script.js
@@ -271,4 +271,16 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
-
+/////////////////----------Forum accordion------------///////////////////////////////
+const acc = document.getElementsByClassName("accordion_forum");
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active_forum");
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
