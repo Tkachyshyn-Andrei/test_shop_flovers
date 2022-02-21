91cc2c338908b3337acf6f9c762fe7d9c840ffe3
Alexandr Shvets <bx2924bk@gmail.com>
2022-02-21 11:32:16 +0200

    small fixes

--- app/js/script.js
+++ app/js/script.js
@@ -261,7 +261,7 @@ function initMap() {
         position: center,
         map,
     });
-}
+};
 
 const swiper = new Swiper('.swiper', {
 
@@ -269,6 +269,40 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
+});
+
+$('.basket-slider').slick({
+    dots: true,
+    // infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
 });
 
 //listener for infotext
